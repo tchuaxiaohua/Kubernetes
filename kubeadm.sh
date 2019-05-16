62ed4775dd7610e8c6ae2814af74be06d4627190
tchuaxiaohua <471445404@qq.com>
2019-05-16 14:51:39 +0800

    初始化测试

--- kubeadm.sh
+++ kubeadm.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
 # 导入依赖
+
+source ../env/env.sh
+
+
+
+
+## 初始化 ##
+
+# network check
+echo "Start testing network connecting..."
+
+for ip  in ${K8S_HOSTS}
+do
+	ping -c 3 -i 0.2 -W 3 $ip >/dev/null 2>&1
+	if [ $? -ne 0 ]
+	then
+	echo "$IP host is faild" && exit -1
+	fi
+done
