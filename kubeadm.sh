443de979690dfb1d860396ac713cdf8db42c97d1
tchuaxiaohua <471445404@qq.com>
2019-05-16 15:50:14 +0800

    修改个别参数错误问题

--- kubeadm.sh
+++ kubeadm.sh
@@ -5,17 +5,17 @@
 source ./env/env.sh
 
 
-# 打印信息
-echo "你正在进行k8s集群操作，集群个节点信息如下:"
+# 打印节点信息
+echo "你正在进行k8s集群部署操作，集群各个节点信息如下:"
 echo "${K8S_MASTER01}:${K8S_MASTER01_IP}"
 echo "${K8S_MASTER02}:${K8S_MASTER02_IP}"
 echo "${K8S_MASTER03}:${K8S_MASTER03_IP}"
 echo "${K8S_NODE01}:${K8S_NODE01_IP}"
-echo "${K8S_NODE01}:${K8S_NODE02_IP}"
+echo "${K8S_NODE02}:${K8S_NODE02_IP}"
 
-## 初始化 ##
+# 初始化 ##
 
-# network check
+## network check
 echo "Start testing network connecting..."
 
 for ip  in ${K8S_HOSTS}
@@ -23,6 +23,6 @@ do
 	ping -c 3 -i 0.2 -W 3 $ip >/dev/null 2>&1
 	if [ $? -ne 0 ]
 	then
-	echo "$IP host is faild" && exit -1
+	echo "$ip host is faild" && exit -1
 	fi
 done
