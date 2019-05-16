68c6fa5e10b19c7a38418beb048615e1cbc06b28
tchuaxiaohua <471445404@qq.com>
2019-05-16 15:35:34 +0800

    节点指定ip

--- kubeadm.sh
+++ kubeadm.sh
@@ -2,10 +2,16 @@
 
 # 导入依赖
 
-source ../env/env.sh
+source ./env/env.sh
 
 
-
+# 打印信息
+echo "你正在进行k8s集群操作，集群个节点信息如下:"
+echo "${K8S_MASTER01}:${K8S_MASTER01_IP}"
+echo "${K8S_MASTER02}:${K8S_MASTER02_IP}"
+echo "${K8S_MASTER03}:${K8S_MASTER03_IP}"
+echo "${K8S_NODE01}:${K8S_NODE01_IP}"
+echo "${K8S_NODE01}:${K8S_NODE02_IP}"
 
 ## 初始化 ##
 
