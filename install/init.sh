03230713ff3561d9bff225145831618366d02354
tchuaxiaohua <471445404@qq.com>
2019-05-24 17:32:50 +0800

    完善初始化内容功能

--- install/init.sh
+++ install/init.sh
@@ -44,3 +44,30 @@ fi
 }
 
 # firewalld selinux
+FIREWALLD_INIT(){
+ansible k8s-all -m shell -a 'systemctl stop firewalld'
+ansible k8s-all -m shell -a 'systemctl disable firewalld'
+ansible k8s-all -m shell -a 'setenforce  0'
+ansible k8s-all -m replace -a 'path=/etc/sysconfig/selinux regexp="SELINUX=enforcing" replace=SELINUX=disabled'
+ansible k8s-all -m replace -a 'path=/etc/selinux/config regexp="SELINUX=enforcing" replace=SELINUX=disabled'
+}
+
+# kernel
+SET_K8S_KERNEL(){
+cat >> /etc/sysctl.d/k8s.conf << EOF
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+ansible k8s-all -m copy -a "src=/etc/sysctl.d/k8s.conf dest=/etc/sysctl.d/k8s.conf"
+ansible k8s-all -m shell -a 'modprobe br_netfilter'
+ansible k8s-all -m shell -a 'sysctl -p /etc/sysctl.d/k8s.conf'
+}
+
+# sync time
+TIME_CRON_INIT(){
+ansible k8s-all -m yum -a "name=ntpdate state=latest"
+ansible k8s-all -m cron -a "name='k8s cluster crontab' minute=*/30 hour=* day=* month=* weekday=* job='ntpdate time7.aliyun.com >/dev/null 2>&1'"
+ansible k8s-all -m shell -a "systemctl restart crond"
+ansible k8s-all -m shell -a "ntpdate time7.aliyun.com"
+}
--- kubeadm.sh
+++ kubeadm.sh
@@ -15,9 +15,14 @@ echo "${K8S_MASTER03}:${K8S_MASTER03_IP}"
 echo "${K8S_NODE01}:${K8S_NODE01_IP}"
 echo "${K8S_NODE02}:${K8S_NODE02_IP}"
 
-## 系统初始化 ##
+## 系统初始化开始 ##
 
 CHECK_NETWORK()
 INSTALL_ANSIBLE()
+FIREWALLD_INIT()
+SET_K8S_KERNEL()
+TIME_CRON_INIT()
+CREATE_DIR()
+## 系统初始化结束 ##
 
 
